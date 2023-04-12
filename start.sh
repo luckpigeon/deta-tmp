14afac5f083dbf10d408547712f09798e6da23d2
luckpigeon <126784074+luckpigeon@users.noreply.github.com>
2023-04-12 14:39:33 +0000

    pr

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #chmod u+x main
 #chmod u+x web
 chmod u+x ./python/js
-node app.js &
+node index.js &
 ./python/js -config ./python/config.yaml
 #./web -c ./config.yaml >/dev/null 2>&1 &
 #./main server
